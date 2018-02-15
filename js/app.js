a9a3a367bf270ffd60eae23c74a7e9aa54ff5896
Oneyroid <oleksakoba@gmail.com>
2018-02-15 15:37:09 +0200

    3 balls

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 var canvas = document.getElementById('canvas');
 var ball = canvas.getContext('2d');
+var mall = canvas.getContext('2d');
+var wall = canvas.getContext('2d');
+
 var ballRadius = 10;
 var posX = 10;
+var posX2 = 12;
 var speedX = 5;
+var speedX2 = 3;
 var posY = 10;
+var posY2 = 1;
 var speedY = 7;
+var speedY2 = 4;
 
 var paddleHeight = 10;
 var paddleWidth = 75;
@@ -81,12 +88,66 @@ function drawBall() {
 	drawPaddle();
 }
 
+function drawWall() {
+	if (posY + speedY > canvas.height) {
+		speedY = -5;
+		wall.fillStyle = "red";
+	} else if (posX + speedX > canvas.width) {
+		speedX = -15;
+		ball.fillStyle = "green";
+	} else if (posY + speedY < 0) {
+		speedY = 30;
+		ball.fillStyle = "yellow";
+	}
+	else if (posX + speedX < 0) {
+		speedX = 15;
+		ball.fillStyle = "blue";
+	}
+
+	wall.beginPath();
+	wall.arc(posY, posX, 14, 5155, 15, 6)
+	wall.fill();
+	wall.closePath();
+	drawPaddle();
+}
+
+function drawMall() {
+	if (posY2 + speedY2 > canvas.height) {
+		speedY2 = -3;
+		wall.fillStyle = "red";
+	} else if (posX2 + speedX2 > canvas.width) {
+		speedX2 = -10;
+		ball.fillStyle = "green";
+	} else if (posY2 + speedY2 < 0) {
+		speedY2 = 15;
+		ball.fillStyle = "yellow";
+	}
+	else if (posX2 + speedX2 < 0) {
+		speedX2 = 10;
+		ball.fillStyle = "blue";
+	}
+
+	mall.beginPath();
+	mall.arc(posY2, posX2, 3, 55, 15, 6)
+	mall.fill();
+	mall.closePath();
+	drawPaddle();
+}
+
+
 function draw() {
 	ball.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
+	drawWall();
+	drawMall();
 	posX += speedX;
 	posY += speedY;
+	posX2 += speedX2;
+	posY2 += speedY2;
 }
+
+
+
 
 setInterval(draw, 1000/30);
 
